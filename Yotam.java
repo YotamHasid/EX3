5fb4e1c7c5241ade2d1d4346c11a7630f7334b6b
YotamHasid <19yotam@gmail.com>
2024-12-13 18:40:55 +0200

    Added Hello.java and Yotam.java

---
+++ Yotam.java
@@ -0,0 +1,7 @@
+public class Yotam {
+    public static void main (string[] args) {
+        for (int i = 0; i < 5; i++){
+            system.out.println("Yotam Hasid");           
+        }
+    }
+}
