a4060380a0c7d34d017fe4e826704d542dceabda
YotamHasid <19yotam@gmail.com>
2024-12-15 13:42:17 +0200

    Updated files

--- Yotam.java
+++ Yotam.java
@@ -1,7 +1,7 @@
 public class Yotam {
     public static void main (String[] args) {
         for (int i = 0; i < 5; i++){
-            System.out.println("My name is Yotam");           
+            System.out.println("My name is Yotam Hasid");           
         }
     }
 }
